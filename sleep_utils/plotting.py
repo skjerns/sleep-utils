fix frequency calculation of multitaper

sleep_utils/plotting.py
@@ -368,9 +368,10 @@ def specgram_multitaper(data, sfreq, sperseg=30, perc_overlap=1/3,
             tick_distance = np.argmax(xy>sperseg*60)*2 #plot per half hour
         two_hz_pos = np.argmax(freq>1.99999999)
         ytick_pos = np.arange(0, len(freq), two_hz_pos)
+        ytick_label = np.linspace(ufreq, lfreq , len(ytick_pos)).round().astype(int)
         ax.set_xticks(np.arange(0, mesh.shape[1], tick_distance))
         ax.set_yticks(ytick_pos)
-        ax.set_yticklabels(np.arange(ufreq, lfreq-1, -2))
+        ax.set_yticklabels(ytick_label)
         ax.set_xlabel('Time after onset')
         ax.set_ylabel('Frequency')
         warnings.filterwarnings("ignore", message='This figure includes Axes that are not compatible')

sleep_utils/tools.py
@@ -139,16 +139,16 @@ def hypno_summary(hypno, epochlen=30, verbose=True):
     lat_S3 = (np.argmax(hypno==3)-onset)*epochlen/60
     lat_REM = (np.argmax(hypno==4)-onset)*epochlen/60
 
-    onset = onset*epochlen/60 # now convert to minutes
-    offset = offset*epochlen/60 # now convert to minutes
+    sleep_onset_after_rec_start = onset*epochlen/60 # now convert to minutes
+    sleep_offset_after_rec_start = offset*epochlen/60 # now convert to minutes
+    recording_length = len(hypno)*epochlen/60
     
     summary = locals().copy()
-    del summary['verbose']
-    del summary['epochlen']
-    del summary['stage']
-    del summary['sleep_stages']
-    del summary['hypno']
-    del summary['num']
+    ignore = ['verbose', 'epochlen', 'stage', 'sleep_stages', 'num', 'hypno',
+              'offset', 'onset']
+    for var in ignore:
+        del summary[var]
+
     if verbose: pprint(summary)
 
     return summary