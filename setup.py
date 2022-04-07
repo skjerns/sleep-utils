31f7df245593a03008c5e5da02b9f0cf04970cfb
skjerns <14980558+skjerns@users.noreply.github.com>
2022-04-07 09:56:32 +0200

    increment version

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(name='sleep-utils',
-      version='1.0',
+      version='1.1',
       description='A collection of tools for sleep research',
       url='http://github.com/skjerns/sleep-utils',
       author='skjerns',
       author_email='nomail@nomail.com',
       license='GNU 2.0',
       packages=['sleep_utils'],
-      install_requires=['mne', 'matplotlib', 'pandas', 'seaborn', 'scipy', 'lspopt', 'scikit-learn', 'numba'],
+      install_requires=['mne', 'matplotlib', 'pandas', 'seaborn', 'scipy', 'lspopt'],
       zip_safe=False)
 
