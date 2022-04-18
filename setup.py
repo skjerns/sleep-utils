6e27accd04a3a8cb70f004897398d35e4bf4a8f8
skjerns <14980558+skjerns@users.noreply.github.com>
2022-04-18 11:23:28 +0200

    add long description

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='sleep-utils',
       version='1.11',
       description='A collection of tools for sleep research',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='http://github.com/skjerns/sleep-utils',
       author='skjerns',
       author_email='nomail@nomail.com',
       license='GNU 2.0',
       packages=['sleep_utils'],
       install_requires=['mne', 'matplotlib', 'pandas', 'seaborn', 'scipy', 'lspopt'],
-      zip_safe=False)
+      zip_safe=False,
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ],)
 
