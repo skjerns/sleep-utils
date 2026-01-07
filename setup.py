73c6eb7212cbee6671936da625b60c90366fcbf9
Simon Kern <14980558+skjerns@users.noreply.github.com>
2026-01-07 07:59:28 +0100

    new release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='sleep_utils',
-      version='1.25',
+      version='1.26',
       description='A collection of tools for sleep research',
       long_description=long_description,
       long_description_content_type="text/markdown",
